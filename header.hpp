362eb39c29bdc83072520306665ed592569782eb
SirFluffi <96303846+SirFluffi@users.noreply.github.com>
2024-08-16 19:44:53 +0200

    Update header.hpp

--- header.hpp
+++ header.hpp
@@ -1,6 +1,12 @@
 #ifndef HEADER_HPP
 #define HEADER_HPP
+#include <future>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <random>
+#include <algorithm>
 
-void dice(int chance, int amount, int instance);
+void dice(int chance, int amount, int instance, std::promise<int> promise);
 
 #endif // !HEADER_HPP
