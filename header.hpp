0fb4f21c6e43cd9c0a1f25b1d09c50796ca0d537
SirFluffi <96303846+SirFluffi@users.noreply.github.com>
2025-03-21 15:53:47 +0100

    Update header.hpp

--- header.hpp
+++ header.hpp
@@ -1,12 +1,12 @@
 #ifndef HEADER_HPP
 #define HEADER_HPP
-#include <future>
+#include <atomic>
 #include <chrono>
 #include <iostream>
 #include <thread>
 #include <random>
 #include <algorithm>
 
-void dice(int chance, unsigned int amount, unsigned int instance, std::promise<int> promise);
+void dice(int chance, unsigned int amount, unsigned int throws, std::atomic<int>& highest);
 
 #endif // !HEADER_HPP
